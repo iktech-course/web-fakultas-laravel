c4b18aaf84f2ed7f61698cca01fddbbc73e3a615
ichsankhaliq99 <ichsankhaliq4292@gmail.com>
2023-08-05 10:45:08 +0700

    fix : adding fix at UserTableSeeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -17,6 +17,13 @@ class UserTableSeeder extends Seeder
             'nama' => 'Irfan Kurniawan',
             'email' => 'irfankurniawan858@gmail.com',
             'password' => bcrypt('IrfanKWN858'),
+            'level' => 2
+        ]);
+
+        User::create([
+            'nama' => 'Ichsan Khaliq Adyanda',
+            'email' => 'ichsankhaliq4292@gmail.com',
+            'password' => bcrypt('Ichsan0109'),
             'level' => 1
         ]);
     }
