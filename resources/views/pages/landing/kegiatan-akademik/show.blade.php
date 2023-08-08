7d931c984f0882961cb87649ae946933e0527855
ichsankhaliq99 <ichsankhaliq4292@gmail.com>
2023-08-08 13:55:33 +0700

    feat: improve

--- resources/views/pages/landing/kegiatan-akademik/show.blade.php
+++ resources/views/pages/landing/kegiatan-akademik/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
         <!-- Hero Start -->
-        <section class="bg-half-170 d-table w-100" style="background: url('assets/images/1undhari/berita.jpg') top;">
+        <section class="bg-half-170 d-table w-100" style="background: url('{{ asset('assets/landing/images/1undhari/berita.jpg') }}') top;">
             <div class="bg-overlay"></div>
             <div class="container">
                 <div class="row mt-5 justify-content-center">
@@ -13,8 +13,8 @@
                             <div class="pages-heading">
                                 <h2 class="text-white title-dark"> Judul Kegiatan Akademik </h2>
                                 <ul class="list-unstyled mt-4">
-                                    <li class="list-inline-item h6 user text-white-50 me-2"><i class="mdi mdi-account"></i> Nama Penulis / Admin</li>
-                                    <li class="list-inline-item h6 date text-white-50"><i class="mdi mdi-calendar-check"></i> 25th June 2021</li>
+                                    <li class="list-inline-item h6 user text-white-50 me-2"><i class="mdi mdi-account"></i> Nama Panitia / Admin</li>
+                                    <li class="list-inline-item h6 date text-white-50"><i class="mdi mdi-calendar-check"></i> Tanggal Kegiatan 25th June 2021</li>
                                 </ul>
                             </div>
                         </div>
@@ -29,152 +29,15 @@
             <div class="container">
                 <div class="row">
                     <!-- BLog Start -->
-                    <div class="col-lg-8 col-md-6">
+                    <div class="col-lg-12 col-md-12">
                         <div class="me-lg-5">
-                            <p class="text-muted">This is required when, for example, the final text is not yet available. Dummy text is also known as 'fill text'. It is said that song composers of the past used dummy texts as lyrics when writing melodies in order to have a 'ready-made' text to sing with the melody.</p>
-                                
                             <ul class="list-unstyled d-flex justify-content-between mt-4">
-                                <li class="list-inline-item user me-2"><a href="javascript:void(0)" class="text-muted"><i class="uil uil-user text-dark"></i> Nama Penulis/Admin</a></li>
-                                <li class="list-inline-item date text-muted"><i class="uil uil-calendar-alt text-dark"></i> 25th June 2021</li>
+                                <li class="list-inline-item user me-2"><a href="javascript:void(0)" class="text-muted"><i class="uil uil-user text-primary"></i><span class="text-primary fw-medium"> Nama Panitia</span></a></li>
+                                <li class="list-inline-item date text-muted"><i class="uil uil-calendar-alt text-primary"></i><span class="text-primary fw-medium"> 25th June 2021</span></li>
                             </ul>
-                            
-                            <img src="assets/images/1undhari/berita.jpg" class="img-fluid rounded-md shadow" alt="">
-
-                            <h5 class="mt-4">Mornings contain the secret to an extraordinarily successful life</h5>
-
+                            <h5 class="mt-4">Sub Judul Kegiatan Akademik</h5>
+                            <p class="text-muted">This is required when, for example, the final text is not yet available. Dummy text is also known as 'fill text'. It is said that song composers of the past used dummy texts as lyrics when writing melodies in order to have a 'ready-made' text to sing with the melody.</p>
                             <p class="text-muted">Due to its widespread use as filler text for layouts, non-readability is of great importance: human perception is tuned to recognize certain patterns and repetitions in texts. If the distribution of letters and 'words' is random, the reader will not be distracted from making a neutral judgement on the visual impact and readability of the typefaces (typography), or the distribution of text on the page (layout or type area).</p>
-
-                            <h5 class="mt-4">Komentar :</h5>
-
-                            <ul class="media-list list-unstyled mb-0">
-                                <li class="mt-4">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="d-flex align-items-center">
-                                            <a class="pe-3" href="#">
-                                                <img src="assets/images/1undhari/profil-default.png" class="img-fluid avatar avatar-md-sm rounded-circle shadow" alt="img">
-                                            </a>
-                                            <div class="flex-1 commentor-detail">
-                                                <h6 class="mb-0"><a href="javascript:void(0)" class="text-dark media-heading">Nama</a></h6>
-                                                <small class="text-muted">15th August, 2021 at 01:25 pm</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-3">
-                                        <p class="text-muted fst-italic p-3 bg-light rounded">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour "</p>
-                                    </div>
-                                </li>
-
-                                <li class="mt-4">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="d-flex align-items-center">
-                                            <a class="pe-3" href="#">
-                                                <img src="assets/images/1undhari/profil-default.png" class="img-fluid avatar avatar-md-sm rounded-circle shadow" alt="img">
-                                            </a>
-                                            <div class="flex-1 commentor-detail">
-                                                <h6 class="mb-0"><a href="javascript:void(0)" class="text-dark media-heading">Nama</a></h6>
-                                                <small class="text-muted">15th August, 2021 at 01:25 pm</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-3">
-                                        <p class="text-muted fst-italic p-3 bg-light rounded">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour "</p>
-                                    </div>
-                                </li>
-
-                                <li class="mt-4">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="d-flex align-items-center">
-                                            <a class="pe-3" href="#">
-                                                <img src="assets/images/1undhari/profil-default.png" class="img-fluid avatar avatar-md-sm rounded-circle shadow" alt="img">
-                                            </a>
-                                            <div class="flex-1 commentor-detail">
-                                                <h6 class="mb-0"><a href="javascript:void(0)" class="text-dark media-heading">Nama</a></h6>
-                                                <small class="text-muted">15th August, 2021 at 01:25 pm</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-3">
-                                        <p class="text-muted fst-italic p-3 bg-light rounded">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour "</p>
-                                    </div>
-                                </li>
-
-                                <li class="mt-4">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="d-flex align-items-center">
-                                            <a class="pe-3" href="#">
-                                                <img src="assets/images/1undhari/profil-default.png" class="img-fluid avatar avatar-md-sm rounded-circle shadow" alt="img">
-                                            </a>
-                                            <div class="flex-1 commentor-detail">
-                                                <h6 class="mb-0"><a href="javascript:void(0)" class="text-dark media-heading">Nama</a></h6>
-                                                <small class="text-muted">15th August, 2021 at 01:25 pm</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-3">
-                                        <p class="text-muted fst-italic p-3 bg-light rounded">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour "</p>
-                                    </div>
-                                </li>
-
-                                <li class="mt-4">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="d-flex align-items-center">
-                                            <a class="pe-3" href="#">
-                                                <img src="assets/images/1undhari/profil-default.png" class="img-fluid avatar avatar-md-sm rounded-circle shadow" alt="img">
-                                            </a>
-                                            <div class="flex-1 commentor-detail">
-                                                <h6 class="mb-0"><a href="javascript:void(0)" class="text-dark media-heading">Nama</a></h6>
-                                                <small class="text-muted">15th August, 2021 at 01:25 pm</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-3">
-                                        <p class="text-muted fst-italic p-3 bg-light rounded">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour "</p>
-                                    </div>
-                                </li>
-
-                            </ul>
-
-                            <h5 class="mt-4">Kirim komen terbaikmu :</h5>
-
-                            <form class="mt-3">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="mb-3">
-                                            <label class="form-label">Komentar</label>
-                                            <div class="form-icon position-relative">
-                                                <i data-feather="message-circle" class="fea icon-sm icons"></i>
-                                                <textarea id="message" placeholder="Tulis Komentar" rows="5" name="message" class="form-control ps-5" required=""></textarea>
-                                            </div>
-                                        </div>
-                                    </div><!--end col-->
-
-                                    <div class="col-lg-6">
-                                        <div class="mb-3">
-                                            <label class="form-label">Nama <span class="text-danger">*</span></label>
-                                            <div class="form-icon position-relative">
-                                                <i data-feather="user" class="fea icon-sm icons"></i>
-                                                <input id="name2" name="name" type="text" placeholder="Nama" class="form-control ps-5" required="">
-                                            </div>
-                                        </div>
-                                    </div><!--end col-->
-
-                                    <div class="col-lg-6">
-                                        <div class="mb-3">
-                                            <label class="form-label">Email <span class="text-danger">*</span></label>
-                                            <div class="form-icon position-relative">
-                                                <i data-feather="mail" class="fea icon-sm icons"></i>
-                                                <input id="email2" type="email" placeholder="Email" name="email" class="form-control ps-5" required="">
-                                            </div>
-                                        </div>
-                                    </div><!--end col-->
-
-                                    <div class="col-md-12">
-                                        <div class="send d-grid">
-                                            <button type="submit" class="btn btn-primary">Kirim Pesan</button>
-                                        </div>
-                                    </div><!--end col-->
-                                </div><!--end row-->
-                            </form><!--end form-->
                         </div>
                     </div><!--end col-->
                     <!-- BLog End -->
